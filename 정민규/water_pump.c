b6bfa4be2ed2975ed32f79bb8e19cf8fc9e70a88
MinGyu2 <mq04034@naver.com>
2021-05-21 03:00:11 +0900

    버튼을 통한 펌프 조절

--- 정민규/water_pump.c
+++ 정민규/water_pump.c
@@ -1,22 +1,15 @@
 #include <includes.h>
 
 static OS_TCB AppTaskStartTCB;
-static OS_TCB AppTask1_TCB;
-static OS_TCB AppTask2_TCB;
-static OS_TCB AppTask3_TCB;
-static OS_TCB AppTask4_TCB;
+static OS_TCB AppWaterPump_TCB;
 
 static CPU_STK AppTaskStartStk[APP_TASK_START_STK_SIZE];
-static CPU_STK AppTask1_Stk[128];
-static CPU_STK AppTask2_Stk[128];
-static CPU_STK AppTask3_Stk[128];
-static CPU_STK AppTask4_Stk[128];
+static CPU_STK AppWaterPump_Stk[128];
 
 static void AppTaskStart (void *p_arg);
-static void AppTask1 (void *p_arg);
-static void AppTask2 (void *p_arg);
-static void AppTask3 (void *p_arg);
-static void AppTask4 (void *p_arg);
+static void AppWaterPumpTask (void *p_arg);
+
+int signal = 0;
 
 int main (void){
   OS_ERR err;
@@ -25,14 +18,15 @@ int main (void){
   
   GPIO_InitTypeDef gpio_init,gpio_init2;
   
+  //led 불빛 task 
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD,ENABLE);//clock give
-  gpio_init.GPIO_Pin = (GPIO_Pin_2 | GPIO_Pin_3| GPIO_Pin_4| GPIO_Pin_7);
+  gpio_init.GPIO_Pin = (GPIO_Pin_2 | GPIO_Pin_3| GPIO_Pin_4| GPIO_Pin_7| GPIO_Pin_11);
   gpio_init.GPIO_Mode = GPIO_Mode_Out_PP;
   gpio_init.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_Init(GPIOD,&gpio_init);
+  //PD10 을 이용하여 water pump 를 조정 한다.
   
-  
-  
+  // 버튼 입력
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE);
   gpio_init2.GPIO_Pin = (GPIO_Pin_2|GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5);
   gpio_init2.GPIO_Mode = GPIO_Mode_IN_FLOATING;
@@ -70,13 +64,14 @@ static void AppTaskStart (void *p_arg){
   cnts = cpu_clk_freq / (CPU_INT32U)OSCfg_TickRate_Hz;
   OS_CPU_SysTickInit(cnts);
   
-  //1
-  OSTaskCreate((OS_TCB *)&AppTask1_TCB,
-               (CPU_CHAR *)"App Task 1",
-               (OS_TASK_PTR)AppTask1,
+  
+  // Water Pump Task
+  OSTaskCreate((OS_TCB *)&AppWaterPump_TCB,
+               (CPU_CHAR *)"Water Pump Task",
+               (OS_TASK_PTR)AppWaterPumpTask,
                (void *) 0,
-               (OS_PRIO) 1,
-               (CPU_STK *) &AppTask1_Stk[0],
+               (OS_PRIO) 5,
+               (CPU_STK *) &AppWaterPump_Stk[0],
                (CPU_STK_SIZE) APP_TASK_START_STK_SIZE/10,
                (CPU_STK_SIZE) APP_TASK_START_STK_SIZE,
                (OS_MSG_QTY) 0,
@@ -85,124 +80,27 @@ static void AppTaskStart (void *p_arg){
                (OS_OPT)(OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR),
                (OS_ERR *) &err);
   
-  //2
-  OSTaskCreate((OS_TCB *)&AppTask2_TCB,
-               (CPU_CHAR *)"App Task 2",
-               (OS_TASK_PTR)AppTask2,
-               (void *) 0,
-               (OS_PRIO) 2,
-               (CPU_STK *) &AppTask2_Stk[0],
-               (CPU_STK_SIZE) APP_TASK_START_STK_SIZE/10,
-               (CPU_STK_SIZE) APP_TASK_START_STK_SIZE,
-               (OS_MSG_QTY) 0,
-               (OS_TICK) 0,
-               (void *) 0,
-               (OS_OPT)(OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR),
-               (OS_ERR *) &err);
-  
-  //3
-  OSTaskCreate((OS_TCB *)&AppTask3_TCB,
-               (CPU_CHAR *)"App Task 3",
-               (OS_TASK_PTR)AppTask3,
-               (void *) 0,
-               (OS_PRIO) 3,
-               (CPU_STK *) &AppTask3_Stk[0],
-               (CPU_STK_SIZE) APP_TASK_START_STK_SIZE/10,
-               (CPU_STK_SIZE) APP_TASK_START_STK_SIZE,
-               (OS_MSG_QTY) 0,
-               (OS_TICK) 0,
-               (void *) 0,
-               (OS_OPT)(OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR),
-               (OS_ERR *) &err);
-  
-  //4
-  OSTaskCreate((OS_TCB *)&AppTask4_TCB,
-               (CPU_CHAR *)"App Task 4",
-               (OS_TASK_PTR)AppTask4,
-               (void *) 0,
-               (OS_PRIO) 4,
-               (CPU_STK *) &AppTask4_Stk[0],
-               (CPU_STK_SIZE) APP_TASK_START_STK_SIZE/10,
-               (CPU_STK_SIZE) APP_TASK_START_STK_SIZE,
-               (OS_MSG_QTY) 0,
-               (OS_TICK) 0,
-               (void *) 0,
-               (OS_OPT)(OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR),
-               (OS_ERR *) &err);
   #if OS_CFG_STAT_TASK_EN > 0u
   OSStatTaskCPUUsageInit(&err);                                 /* Compute CPU capacity with no task running        */
 #endif
 
   CPU_IntDisMeasMaxCurReset();
   
-  GPIO_ResetBits(GPIOD,(GPIO_Pin_2|GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_7));
   while(DEF_TRUE){
     OSTimeDlyHMSM(0,0,0,100,
                   OS_OPT_TIME_HMSM_STRICT,
                   &err);
   }
 }
-
-
-static void AppTask1(void *p_arg){
+static void AppWaterPumpTask (void *p_arg){
   OS_ERR err;
   p_arg = p_arg;
   while(1){
-    //GPIO_SetBits(GPIOD,GPIO_Pin_2);
-    OSTimeDlyHMSM(0,0,0,100,
-                  (OS_OPT) OS_OPT_TIME_HMSM_STRICT,
-                  (OS_ERR *)&err);
-    //GPIO_ResetBits(GPIOD,GPIO_Pin_2);
-    OSTimeDlyHMSM(0,0,0,100,
-                  (OS_OPT) OS_OPT_TIME_HMSM_STRICT,
-                  (OS_ERR *)&err);
-  }
-}
-static void AppTask2(void *p_arg){
-  OS_ERR err;
-  p_arg = p_arg;
-  u8 a = 0;
-  while(1){
-    a = GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_2); // down
-    if(a){
-      GPIO_SetBits(GPIOD,GPIO_Pin_3);
-      GPIO_ResetBits(GPIOD, GPIO_Pin_2);
+    signal = GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_2); // down
+    if(signal == 0){
+      GPIO_ResetBits(GPIOD, GPIO_Pin_11);
     }else{
-      GPIO_ResetBits(GPIOD, GPIO_Pin_3);
-      GPIO_SetBits(GPIOD,GPIO_Pin_2);
+      GPIO_SetBits(GPIOD,GPIO_Pin_11);
     }
-    OSTimeDlyHMSM(0,0,0,200,
-                  (OS_OPT) OS_OPT_TIME_HMSM_STRICT,
-                  (OS_ERR *)&err);
-  }
-}
-static void AppTask3(void *p_arg){
-  OS_ERR err;
-  p_arg = p_arg;
-  
-  while(1){
-    GPIO_SetBits(GPIOD,GPIO_Pin_4);
-    OSTimeDlyHMSM(0,0,0,500,
-                  (OS_OPT) OS_OPT_TIME_HMSM_STRICT,
-                  (OS_ERR *)&err);
-    GPIO_ResetBits(GPIOD,GPIO_Pin_4);
-    OSTimeDlyHMSM(0,0,0,500,
-                  (OS_OPT) OS_OPT_TIME_HMSM_STRICT,
-                  (OS_ERR *)&err);
-  }
-}
-static void AppTask4(void *p_arg){
-  OS_ERR err;
-  p_arg = p_arg;
-  
-  while(1){
-    GPIO_SetBits(GPIOD,GPIO_Pin_7);
-    OSTimeDlyHMSM(0,0,1,0,
-                  (OS_OPT) OS_OPT_TIME_HMSM_STRICT,
-                  (OS_ERR *)&err);
-    GPIO_ResetBits(GPIOD,GPIO_Pin_7);
-    OSTimeDlyHMSM(0,0,1,0,
-                  (OS_OPT) OS_OPT_TIME_HMSM_STRICT,
-                  (OS_ERR *)&err);
   }
 }
