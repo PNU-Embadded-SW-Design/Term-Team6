ee8017998fd51ad05e21dd40e117eccbe3a9fbf2
MinGyu2 <mq04034@naver.com>
2021-06-06 02:40:52 +0900

    수위 센서 코드 정리 및 일정 수위 넘으면 led on

--- 정민규/water_sensor.c
+++ 정민규/water_sensor.c
@@ -13,7 +13,8 @@ static void AppWaterSensorTask (void *p_arg);
 static void AppTask1 (void *p_arg); // test task
 
 void write(int n);
-
+void USART_OptionInit();
+void WaterSensor_Init();
 
 int signal = 0;
 
@@ -21,27 +22,6 @@ int main (void){
   OS_ERR err;
   BSP_IntDisAll();
   OSInit(&err);
-  
-  GPIO_InitTypeDef gpio_init,gpio_init2;
-  
-  //led 불빛 task 
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD,ENABLE);//clock give
-  //RCC_APB2Periph_ADC1
-  gpio_init.GPIO_Pin = (GPIO_Pin_2);
-  gpio_init.GPIO_Mode = GPIO_Mode_Out_PP;
-  gpio_init.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOD,&gpio_init);
-  //PD11 을 이용하여 water pump 를 조정 한다.
-  
-  // 버튼 입력
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE);
-  gpio_init2.GPIO_Pin = (GPIO_Pin_2|GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5);
-  gpio_init2.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-  gpio_init2.GPIO_Speed = GPIO_Speed_2MHz;
-  GPIO_Init(GPIOC,&gpio_init2);
-  
-  
-  
   
   OSTaskCreate((OS_TCB     *)&AppTaskStartTCB,                /* Create the start task                                */
                  (CPU_CHAR   *)"App Task Start",
@@ -74,58 +54,27 @@ static void AppTaskStart (void *p_arg){
   cnts = cpu_clk_freq / (CPU_INT32U)OSCfg_TickRate_Hz;
   OS_CPU_SysTickInit(cnts);
   
-  //ADC 설정
-  //RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE);//clock give
-  GPIO_InitTypeDef gpio_init;
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1,ENABLE);
-  gpio_init.GPIO_Pin = GPIO_Pin_5;
-  gpio_init.GPIO_Mode = GPIO_Mode_AIN;
-  GPIO_Init(GPIOC,&gpio_init);
+  GPIO_InitTypeDef gpio_init,gpio_init2;
   
-  ADC_InitTypeDef adc_init;
-  adc_init.ADC_Mode = ADC_Mode_Independent;
-  adc_init.ADC_ScanConvMode = DISABLE;
-  adc_init.ADC_ContinuousConvMode = DISABLE;
-  adc_init.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
-  adc_init.ADC_NbrOfChannel = 1;
-  ADC_Init(ADC1,&adc_init);
-  
-  ADC_RegularChannelConfig(ADC1,ADC_Channel_15,1,ADC_SampleTime_55Cycles5);
-  
-  ADC_Cmd(ADC1,ENABLE);
-  
-  ADC_ResetCalibration(ADC1);
-  while(ADC_GetResetCalibrationStatus(ADC1));
-  
-  ADC_StartCalibration(ADC1);
-  while(ADC_GetCalibrationStatus(ADC1));
-  
-  
-  
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
-  
-  //TXD write pin9
-  gpio_init.GPIO_Mode = GPIO_Mode_AF_PP;
-  gpio_init.GPIO_Pin = GPIO_Pin_9;
+  //led 불빛 task 
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD,ENABLE);//clock give
+  gpio_init.GPIO_Pin = (GPIO_Pin_2);//GPIO_Pin_11 pump
+  gpio_init.GPIO_Mode = GPIO_Mode_Out_PP;
   gpio_init.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOA,&gpio_init);
+  GPIO_Init(GPIOD,&gpio_init);
   
-  //Rx read pin10
-  gpio_init.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-  gpio_init.GPIO_Pin = GPIO_Pin_10;
-  GPIO_Init(GPIOA,&gpio_init);
+  //PD11 을 이용하여 water pump 를 조정 한다.
   
-  //uart1 Task
-  USART_InitTypeDef uart_init;
-  uart_init.USART_BaudRate = 9600;
-  uart_init.USART_WordLength = USART_WordLength_8b;
-  uart_init.USART_StopBits = USART_StopBits_1;
-  uart_init.USART_Parity = USART_Parity_No;
-  uart_init.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-  uart_init.USART_Mode = USART_Mode_Rx|USART_Mode_Tx;
-  USART_Init(USART1,&uart_init);
-  USART_Cmd(USART1,ENABLE);
+  // 버튼 입력
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE);
+  gpio_init2.GPIO_Pin = (GPIO_Pin_2|GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5);
+  gpio_init2.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  gpio_init2.GPIO_Speed = GPIO_Speed_2MHz;
+  GPIO_Init(GPIOC,&gpio_init2);
+  
+  WaterSensor_Init();
+  USART_OptionInit();
+  
   
   // task 를 round robin 으로 하시 위해서 
   OSSchedRoundRobinCfg((CPU_BOOLEAN)DEF_TRUE, 
@@ -171,7 +120,6 @@ static void AppTaskStart (void *p_arg){
   
   while(DEF_TRUE){
     while(!USART_GetFlagStatus(USART1,USART_FLAG_TXE));
-    //USART_SendData(USART1,'a');
     OSTimeDlyHMSM(0,0,0,100,
                   OS_OPT_TIME_HMSM_STRICT,
                   &err);
@@ -189,12 +137,14 @@ static void AppWaterSensorTask (void *p_arg){
     
     signal = adc_value;
     
-    signal = GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_2); // down
+    signal = GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_2); // button down
     write(adc_value);
-    if(signal == 0){
-      GPIO_ResetBits(GPIOD, GPIO_Pin_11);
+    if(adc_value > 1200){
+      //GPIO_ResetBits(GPIOD, GPIO_Pin_11);
+      GPIO_ResetBits(GPIOD, GPIO_Pin_2);
     }else{
-      GPIO_SetBits(GPIOD,GPIO_Pin_11);
+      //GPIO_SetBits(GPIOD,GPIO_Pin_11);
+      GPIO_SetBits(GPIOD,GPIO_Pin_2);
     }
   }
 }
@@ -210,6 +160,62 @@ static void AppTask1 (void *p_arg){
     }
     ccc +=1;
   }
+}
+
+void WaterSensor_Init(){
+  GPIO_InitTypeDef gpio_init;
+  //ADC 설정
+  //RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE);//clock give
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1,ENABLE);
+  gpio_init.GPIO_Pin = GPIO_Pin_5;
+  gpio_init.GPIO_Mode = GPIO_Mode_AIN;
+  GPIO_Init(GPIOC,&gpio_init);
+  
+  ADC_InitTypeDef adc_init;
+  adc_init.ADC_Mode = ADC_Mode_Independent;
+  adc_init.ADC_ScanConvMode = DISABLE;
+  adc_init.ADC_ContinuousConvMode = DISABLE;
+  adc_init.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+  adc_init.ADC_NbrOfChannel = 1;
+  ADC_Init(ADC1,&adc_init);
+  
+  ADC_RegularChannelConfig(ADC1,ADC_Channel_15,1,ADC_SampleTime_55Cycles5);
+  
+  ADC_Cmd(ADC1,ENABLE);
+  
+  ADC_ResetCalibration(ADC1);
+  while(ADC_GetResetCalibrationStatus(ADC1));
+  
+  ADC_StartCalibration(ADC1);
+  while(ADC_GetCalibrationStatus(ADC1));
+}
+
+void USART_OptionInit(){
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
+  
+  GPIO_InitTypeDef gpio_init;
+  //TXD write pin9
+  gpio_init.GPIO_Mode = GPIO_Mode_AF_PP;
+  gpio_init.GPIO_Pin = GPIO_Pin_9;
+  gpio_init.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOA,&gpio_init);
+  
+  //Rx read pin10
+  gpio_init.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  gpio_init.GPIO_Pin = GPIO_Pin_10;
+  GPIO_Init(GPIOA,&gpio_init);
+  
+  //uart1 Task
+  USART_InitTypeDef uart_init;
+  uart_init.USART_BaudRate = 9600;
+  uart_init.USART_WordLength = USART_WordLength_8b;
+  uart_init.USART_StopBits = USART_StopBits_1;
+  uart_init.USART_Parity = USART_Parity_No;
+  uart_init.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  uart_init.USART_Mode = USART_Mode_Rx|USART_Mode_Tx;
+  USART_Init(USART1,&uart_init);
+  USART_Cmd(USART1,ENABLE);
 }
 void write(int n){
   char c=0x30;
